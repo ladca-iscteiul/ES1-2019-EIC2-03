21f154dffb5ac54a8dc8b8b2b195fdc2cc46ec52
ladca-iscteiul <ladca@iscte-iul.pt>
2019-12-01 18:43:46 +0000

    Maven porject created

---
+++ DefectsDetection/src/main/java/ES1_2019_EIC2_03/DefectsDetection/App.java
@@ -0,0 +1,13 @@
+package ES1_2019_EIC2_03.DefectsDetection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
