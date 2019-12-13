6f1380c9a691d5254456d89d8fcf4568b042740c
ladca-iscteiul <ladca@iscte-iul.pt>
2019-12-13 17:22:11 +0000

    Correção de erros nas classes de testes

--- DefectsDetection/src/test/java/ES1_2019_EIC2_03/DefectsDetection/code/AllTests.java
+++ DefectsDetection/src/test/java/ES1_2019_EIC2_03/DefectsDetection/code/AllTests.java
@@ -1,12 +1,13 @@
 package ES1_2019_EIC2_03.DefectsDetection.code;
 
 import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(JUnitPlatform.class)
-@SuiteClasses({CostumRuleTest.class, DefectsDetectionProgramTest.class, DefectsTest.class, ExcelExporterTest.class})
+@SelectClasses({CostumRuleTest.class, DefectsDetectionProgramTest.class, DefectsTest.class, ExcelExporterTest.class})
 public class AllTests {
 
 }
